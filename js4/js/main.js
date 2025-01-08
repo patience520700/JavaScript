36bea7836a77a48a0dc6883e1ae91d123d63da0b
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-08 19:40:33 +0000

    Add example using Number.isInteger() to check if a float is an integer

--- js4/js/main.js
+++ js4/js/main.js
@@ -7,4 +7,4 @@ const myString = "42";
 // Number Methods
 // A Number.isInteger() method determines whether the passed value is an integer
 
-console.log(Number.isInteger( myNumber));
+console.log(Number.isInteger( myFloat));
