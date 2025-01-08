8da3ae9d30b9e6769e8478c9b9b4af02974bfaa5
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-08 19:37:54 +0000

    Add example using Number.isInteger()

--- js4/js/main.js
+++ js4/js/main.js
@@ -1,11 +1,10 @@
 // Numbers
 
-// An integer is a whole number.
 const myNumber = 42;
-
-// A number with a decimal point is a float which references the "floating point"
-const myFloat = 42.0;
-
+const myFloat = 42.01;
 const myString = "42";
 
-console.log(Number(false));
+// Number Methods
+// A Number.isInteger() method determines whether the passed value is an integer
+
+console.log(Number.isInteger( myNumber));
