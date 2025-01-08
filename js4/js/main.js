0434ccf25f7fb91b698ec2cb3eaf7122d29bf803
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-08 19:04:24 +0000

    Add example comparing myNumber to myFloat

--- js4/js/main.js
+++ js4/js/main.js
@@ -4,8 +4,8 @@
 const myNumber = 42;
 
 // A number with a decimal point is a float which references the "floating point"
-const myFloat = 42.01;
+const myFloat = 42.0;
 
 const myString = "42"
 
-console.log(myString);
+console.log(myNumber == myFloat);
