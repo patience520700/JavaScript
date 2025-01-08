0f947335d47e222e4069a45e71d938ce65201244
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-08 19:50:52 +0000

    Add example using Number.parseFloat() with an integer value

--- js4/js/main.js
+++ js4/js/main.js
@@ -2,7 +2,7 @@
 
 const myNumber = 42;
 const myFloat = 42.01;
-const myString = "42";
+const myString = "42.123abc";
 
 // Number Methods
 // A Number.isInteger() method determines whether the passed value is an integer
