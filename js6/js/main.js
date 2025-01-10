6481360a2ec44f6b96a697281f52a18c27168c2a
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-10 19:32:18 +0000

    Add code to return a random letter from the name 'Akos' using `charAt()` and `Math.random()`

---
+++ js6/js/main.js
@@ -0,0 +1,8 @@
+// my First code Challenge
+
+// write code that will return a random letter from my name
+
+console.log("Akos".charAt(Math.floor(Math.random() * 4)));
+console.log("Akos".charAt(Math.floor(Math.random() * 4)));
+console.log("Akos".charAt(Math.floor(Math.random() * 4)));
+console.log("Akos".charAt(Math.floor(Math.random() * 4)));
