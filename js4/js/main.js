9a683e16a4f8f2fdc0dcca124b755028a1873ef0
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-10 18:21:44 +0000

    Add example demonstrating method chaining: `Number.parseFloat()`, `toFixed()`, and `toString()`

--- js4/js/main.js
+++ js4/js/main.js
@@ -4,9 +4,6 @@ const myNumber = 42;
 const myFloat = 42.0151;
 const myString = "42.123abc";
 
-// Number Methods
-// A Number.isInteger() method determines whether the passed value is an integer
+//Chasing = Using several methods chained together
 
-// The toString() method returns a string representing the number.
-
-console.log(typeof Number.parseFloat(myString));
+console.log(Number.parseFloat("4.237abc").toFixed(2).toString());
