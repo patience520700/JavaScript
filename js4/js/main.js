7898af104f26e0474441734fbd279b1a5e7c311c
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-10 17:49:16 +0000

    Add example using `Number.parseInt()` to parse a float into an integer

--- js4/js/main.js
+++ js4/js/main.js
@@ -7,8 +7,6 @@ const myString = "42.123abc";
 // Number Methods
 // A Number.isInteger() method determines whether the passed value is an integer
 
-// The Number.parseFloat() method parses an argument and returns a floating point number. If a number cannot be parsed from the argument, it returns NaN.
+// The Number.parseInt() method parses a string argument and returns an integer
 
-// The tofixed() method formats a number according to how many decimal points you provide as the parameter.
-
-console.log(myFloat.toFixed(2));
+console.log(Number.parseInt(myFloat));
