973322854a14756b0c44386b83525417c9f7c812
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-10 18:29:14 +0000

    Add example using `Number.isNaN()` to check if a non-numeric string results in NaN

--- js4/js/main.js
+++ js4/js/main.js
@@ -2,4 +2,6 @@
 
 //NaN is an acronym for Not a Number
 
-console.log(Number("Pat"));
+//The Number.isNaN() method determines whether the passed value is NaN and its type is Number.
+
+console.log(Number.isNaN("Pat"));
