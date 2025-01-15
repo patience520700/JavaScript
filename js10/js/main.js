ab21b8f3b3f0d54fcc0a1200c67e3c0160592302
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-15 19:10:29 +0000

    Add example to handle user input: check name length, trim whitespace, and handle empty input

--- js10/js/main.js
+++ js10/js/main.js
@@ -1,4 +1,9 @@
 // User Input
 let name = prompt("Please enter your name.");
-console.log(typeof name);
-console.log(name ?? "You didn't enter your name.");
+if (name) {
+  console.log(name.length);
+  console.log(name.trim().length);
+  console.log(name.trim());
+} else {
+    console.log("You didn't enter your name.");
+}
