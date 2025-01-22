5b91b2b9dd8df9750bec32a72d559893e90b144b
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-22 18:10:48 +0000

    Add while loop with conditional logic and control statements

---
+++ js13/js/main.js
@@ -0,0 +1,12 @@
+// Functions
+
+// Function Declaration Syntax:
+
+function sum(num1, num2) {
+    if (num2 === undefined) {
+        return num1 + num1;
+    }
+    return num1 + num2;
+}
+
+console.log(sum(5, 10));
