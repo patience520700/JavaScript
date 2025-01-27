cbc94e7d89874c8df3ed2e51276155b8d833d336
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-27 19:55:47 +0000

    Add example of array manipulation using indexing and splice

---
+++ js15/js/main.js
@@ -0,0 +1,12 @@
+// Arrays
+const myArray = [];
+
+// add elements to an array
+myArray[0] = "Vero";
+myArray[1] = 1001;
+myArray[2] = false;
+
+myArray.splice(1, 0, 42);
+
+console.log(myArray);
+console.log(myArray[1]);
