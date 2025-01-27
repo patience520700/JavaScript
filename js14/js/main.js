4d4963b76a0e4dbb08faff0813bcc24e8976be82
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-27 18:43:19 +0000

    Add example illustrating global and local scopes with var, let, and const

--- js14/js/main.js
+++ js14/js/main.js
@@ -1,20 +1,35 @@
 // var, let, and const
 
 // global scope (is a variable to all)
-var x = 1;
-let y = 2;
+var x = 1; // function scoped
+let y = 2; // block scoped
 const z = 3;
 
-// local scope
-function myFunc() {
-    const z = 5;
-    console.log(y);
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${z}`);
 
-    // local scope
+function myFunc() {
+    var x = 10;
+    const z = 5;
+    // console.log(`function: ${x}`);
+    // console.log(`function: ${y}`);
+    // console.log(`function: ${z}`);
     {
-        let y = 4;
-        console.log(y);
+        var x = 11; // function scoped
+        const z = 6; // block scoped
+       console.log(`block: ${x}`);
+       console.log(`block: ${y}`);
+       console.log(`block: ${z}`);
     }
-} 
+    console.log(`function: ${x}`);
+    console.log(`function: ${y}`);
+    console.log(`function: ${z}`);
+}
 
 myFunc();
+
+// Global = Not in a function or block.Not desirable.
+// Local = In a function or block.Not global.
+// Var = insantiates function() scoped variables.
+// let and const instantiate {block} scoped variables.z
