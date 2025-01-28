e521ca8b1559b8076a6821f93c054466a754e547
Patience Amegayi <akhosuahpatience@gmail.com>
2025-01-28 23:32:41 +0000

    Add example of multi-dimensional arrays representing a sports store layout

--- js15/js/main.js
+++ js15/js/main.js
@@ -1,12 +1,21 @@
 // Arrays
-const myArray = [];
 
-// add elements to an array
-myArray[0] = "Vero";
-myArray[1] = 1001;
-myArray[2] = false;
+const equipShelfA = ["baseball", "football", "volleyball"];
+const equipShelfB = ["basketball", "golf balls", "tennis balls"];
 
-myArray.splice(1, 0, 42);
+const clothesShelfA = ["tank tops", "t-shirts", "jerseys"];
+const clothesShelfB = ["sweat tops", "sweat pants", "hoodies"];
 
-console.log(myArray);
-console.log(myArray[1]);
+console.log(equipShelfA[1]);
+//or
+console.log(clothesShelfB[0]);
+
+const equipDept = [equipShelfA, equipShelfB];
+const clothesDept = [clothesShelfA, clothesShelfB];
+
+console.log(equipDept[0][1]);
+console.log(clothesDept[1][0]);
+
+const sportsStore = [equipDept, clothesDept];
+console.log(sportsStore[0][0][1]);
+console.log(sportsStore[1][1][0]);
