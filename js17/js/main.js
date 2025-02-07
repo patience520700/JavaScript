a24dea149a6e8e15ca8841f5393b298e11b62dc8
Patience Amegayi <akhosuahpatience@gmail.com>
2025-02-07 18:09:15 +0000

    Add example demonstrating object destructuring in a function parameter

--- js17/js/main.js
+++ js17/js/main.js
@@ -6,13 +6,10 @@ const band = {
     bass: "John Paul Jones",
     drums: "John Bonham"
 };
+// destructuring objects
 
-delete band.drums;
-console.log(band.hasOwnProperty("drums"));
-
-console.log(Object.keys(band));
-console.log(Object.values(band));
-
-for (let job in band) {
-    console.log(`On ${job}, it's ${band[job]}!`);
+function sings({ vocals }) { 
+    return `${vocals} sings!`;
 }
+
+console.log(sings(band));
