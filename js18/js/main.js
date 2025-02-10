f65e1953ad23bad8977a16dfd68cd1a7c053bd27
Patience Amegayi <akhosuahpatience@gmail.com>
2025-02-10 13:50:48 +0000

    Add example demonstrating JavaScript classes with private fields, getters, setters, and methods

--- js18/js/main.js
+++ js18/js/main.js
@@ -1,10 +1,23 @@
+/* // Factory Function
+function pizzaFactory(pizzaSize) {
+    const crust = "original";
+    const size = pizzaSize;
+    return {
+        bake: () => console.log(`Baking a ${size} ${crust} crust pizza.`)
+    };
+}
+
+const myPizza = pizzaFactory("small");
+myPizza.bake();
+ */
+
 // Javascript Classes
 class Pizza {
-    constructor(pizzaType, pizzaSize) {
-        this.type = pizzaType;
-        this.size = pizzaSize;
-        this.crust = "original";
-        this.toppings = [];
+    crust = "original";
+    #sauce = "traditional";
+    #size;
+    constructor(pizzaSize) {
+        this.#size = pizzaSize;
     }
     getCrust() {
         return this.crust;
@@ -12,22 +25,12 @@ class Pizza {
     setCrust(pizzaCrust) {
         this.crust = pizzaCrust;
     }
-    getToppings() {
-        return this.toppings;
-    }
-    setToppings(topping) {
-        this.toppings.push(topping);
-    }
-    bake() {
+    hereYouGo() {
         console.log(
-            `Baking a ${this.size} ${this.type} ${this.crust} crust pizza.`
+            `Here's your ${this.crust} ${this.#sauce} sauce ${this.#size} pizza.`
         );
     }
 }
-
-const myPizza = new Pizza("pepperoni", "small");
-myPizza.setCrust("thin");
-myPizza.bake();
-myPizza.setToppings("sausage");
-myPizza.setToppings("olives");
-console.log(myPizza.getToppings());
+const myPizza = new Pizza("large");
+myPizza.hereYouGo();
+// console.log(myPizza.crust);
