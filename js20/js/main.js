983348a4b0591f8fa79b46ba297f46791cfe72fb
Patience Amegayi <akhosuahpatience@gmail.com>
2025-02-10 19:35:50 +0000

    Add JavaScript Errors and Error Handling

--- js20/js/main.js
+++ js20/js/main.js
@@ -1,18 +1,19 @@
 // JavaScript Errors and Error Handling
 "use strict";
 const makeError = () => {
-    try {
-       throw new customError("This is a custom error!");
-    }catch(err) {
-        console.error(err.name);
-        console.error(err.message);
-        console.error(err.stack);
+    let i = 1;
+    while (i <= 5) {
+        try {
+            if (i % 2 !== 0) {
+                throw new Error("Odd number!");
+            }
+            console.log("Even number!");
+        }   catch (err) {
+            console.error(err.stack);
+        }   finally {
+            console.log("...finally");
+            i++;
+        }
     }
 }
 makeError();
-
-function customError(message) {
-    this.message = message;
-    this.name = "CustomError";
-    this.stack = `${this.name}: ${this.message}`;
-}
