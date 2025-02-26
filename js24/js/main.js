ade60f2a10c9d4253cffa126ca23af0707a6edef
Patience Amegayi <akhosuahpatience@gmail.com>
2025-02-26 22:07:08 +0000

    Added module imports: Integrated guitars and user modules with import/export functionality.

---
+++ js24/js/main.js
@@ -0,0 +1,10 @@
+// Modules
+import * as Guitars from "./guitars.js";
+import User from "./user.js";
+const me = new User("email@email.com", "Dave");
+console.log(me);
+console.log(me.greeting());
+
+console.log(Guitars.playGuitar());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
