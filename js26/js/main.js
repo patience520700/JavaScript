398418edeabd0ce31ca64db9cf29eb8ed4c35df9
Patience Amegayi <akhosuahpatience@gmail.com>
2025-02-28 19:34:22 +0000

    Add Abstracted request handling into modular functions with async/await

--- js26/js/main.js
+++ js26/js/main.js
@@ -1,60 +1,39 @@
 // Fetch API requires a discussion of...
 // Callbacks, Promises, Thenables, Async/Await
 
-//Promises
+// abstract into functions
 
-// 3 steps: Pending, Fulfilled, Rejected
+// maybe from a form 
+const getDataFromForm = () => {
+   const requestObj = {
+    firstName: "Bruce",
+    lastName: "Lee",
+    categories: ["nerdy"]
+   };
+   return requestObj;
+}
 
-const users = fetch("https://jsonplaceholder.typicode.com/users");
+const buildRequestUrl = (requestdata) => {
+    return `http://api.icndb.com/jokes/random?firstName=${requestdata.firstName}&lastName=${requestdata.lastName}& limitTo={requestData.categories}`;
+}
 
-// pending
-console.log(users);
+const requestJoke = async (url) => {
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    const joke = jsonResponse.value.joke;
+    postJokeToPage(joke);
+}
 
-fetch("https://jsonplaceholder.typicode.com/users")
-   .then(response => {
-    return response.json();
-   })
-   .then(data => {
-    data.forEach(user => {
-        console.log(user);
-    })
-   });
+const postJokeToPage = (joke) => {
+    console.log(joke);
+}
 
+// Procedural "workflow" function
+const processJokeRequest = async () => {
+    const requestData = getDataFromForm();
+    const requestUrl = buildRequestUrl(requestData);
+    await requestJoke(requestUrl);
+    console.log("finished!");
+}
 
-/* const myPromise = new Promise((resolve, reject) => {
-    const error = false;
-    if (!error) {
-        resolve("Yes! resolved the promise!");
-    } else {
-        reject("No! rejected the promise!");
-    }
-});
- */
-/* console.log(myPromise);
-
-myPromise
-.then(value => {
-    return value + 1;
-})
-.then(newValue => {
-    console.log(newValue);
-})
-.catch(err => {
-    console.error(err);
-})
- */
-
-/* const myNextPromise = new Promise((resolve, reject) => {
-    setTimeout(function() {
-        resolve("myNewPromise resolved!");
-    }, 3000);
-});
-
-myNextPromise.then(value => {
-    console.log(value);
-});
-
-myPromise.then(value => {
-    console.log(value);
-});
- */
+processJokeRequest();
